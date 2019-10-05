c879bb6080d5ca2e30a4e2770f24f1c3695c70b6
Andrés Felipe Ruiz Velasco <ruiz109069@siigo.com>
2019-10-05 16:53:28 -0500

    Add CORS to api config

--- Back/InsurancesAPI/InsurancesAPI/App_Start/WebApiConfig.cs
+++ Back/InsurancesAPI/InsurancesAPI/App_Start/WebApiConfig.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace InsurancesAPI
 {
@@ -15,6 +16,8 @@ namespace InsurancesAPI
 
             // Web API routes
             config.MapHttpAttributeRoutes();
+
+            config.EnableCors(new EnableCorsAttribute("*", "*", "*"));
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
